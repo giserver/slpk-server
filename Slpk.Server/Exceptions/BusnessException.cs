33824d03dce0a141d615d4b2d229ed210236c34a
tracy <tracywang1997@outlook.com>
2022-02-21 13:52:16 +0800

    增加对slpk解压文件夹的服务解析

--- Slpk.Server/Exceptions/BusnessException.cs
+++ Slpk.Server/Exceptions/BusnessException.cs
@@ -2,11 +2,16 @@
 
 namespace Slpk.Server.Exceptions
 {
-    public class BusnessException:Exception
+    public class BusnessException : Exception
     {
         public int StatusCode { get; }
 
-        public BusnessException(int statusCode ,string message):base(message)
+        public BusnessException(int statusCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+
+        public BusnessException(int statusCode, string message, Exception? innerException) : base(message, innerException)
         {
             StatusCode = statusCode;
         }
--- Slpk.Server/Services/SlpkFileService.cs
+++ Slpk.Server/Services/SlpkFileService.cs
@@ -5,51 +5,109 @@ namespace Slpk.Server.Services
 {
     public interface ISlpkFileService
     {
-        public string GetFullPath(string fileName);
+        /// <summary>
+        /// 获取项目入口完整路径
+        /// 
+        /// </summary>
+        /// <param name="entryName">入口名，文件夹名称(.slpk解压文件夹名称)，亦 .slpk文件</param>
+        /// <returns></returns>
+        public string GetFullPath(string entryName);
 
-        Task<byte[]?> ReadAsync(string filePath, string innerPath, bool deGzip = false);
+        /// <summary>
+        /// 读取项目文件
+        /// </summary>
+        /// <param name="projectPath">入口路径</param>
+        /// <param name="innerPath">内部路径</param>
+        /// <param name="deGzip">是否对Gzip类型文件解压</param>
+        /// <returns></returns>
+        Task<byte[]?> ReadAsync(string entryPath, string innerPath, bool deGzip = false);
     }
 
     public class SlpkFileService : ISlpkFileService
     {
         private readonly IConfiguration configuration;
+        private readonly ILogger<SlpkFileService> logger;
 
-        public SlpkFileService(IConfiguration configuration)
+        public SlpkFileService(IConfiguration configuration,ILogger<SlpkFileService> logger)
         {
             this.configuration = configuration;
+            this.logger = logger;
         }
 
-        public string GetFullPath(string fileName)
+        public string GetFullPath(string entryName)
         {
-            var fullPath = Path.Combine(configuration.GetSection("Slpk:Dir").Value, fileName);
-            if (!File.Exists(fullPath))
-                throw new BusnessException(404, $"Can't found SLPK: {fileName}");
+            var fullPath = Path.Combine(configuration.GetSection("Slpk:Dir").Value, entryName);
+            if (!ExistPath(fullPath,out var _))
+                throw new BusnessException(404, $"Can't found SLPK: {entryName}");
 
-            return fullPath; 
+            return fullPath;
         }
 
-        public async Task<byte[]?> ReadAsync(string filePath, string innerPath, bool deGzip = false)
+        public async Task<byte[]?> ReadAsync(string entryPath, string innerPath, bool deGzip = false)
         {
-            if (!File.Exists(filePath)) return null;
+            if (!ExistPath(entryPath,out var isFile)) return null;
+            
+            // 获取对应文件的stream
+            Stream? stream;
+            ZipArchive? zip = null;
 
-            using var zip = ZipFile.Open(filePath, ZipArchiveMode.Read);
-            var entry = zip.GetEntry(innerPath);
-
-            if (entry == null) return null;
-
-            using var stream = entry.Open();
-
-            if (deGzip)
+            if (isFile)
             {
-                using var destStream = new MemoryStream();
-                using var srcStream = new GZipStream(stream, CompressionMode.Decompress);
-                await srcStream.CopyToAsync(destStream);
-                return destStream.ToArray();
+                zip = ZipFile.Open(entryPath, ZipArchiveMode.Read);
+                var entry = zip.GetEntry(innerPath);
+                if (entry == null) return null;
+
+                stream = entry.Open();
+            }
+            else
+                stream = File.OpenRead(Path.Combine(entryPath, innerPath));
+
+            // 读取文件内容
+            try
+            {
+                // 对".gz"压缩文件选择式解压
+                if (deGzip && innerPath.EndsWith(".gz"))
+                {
+                    using var destStream = new MemoryStream();
+                    using var srcStream = new GZipStream(stream, CompressionMode.Decompress);
+                    await srcStream.CopyToAsync(destStream);
+                    return destStream.ToArray();
+                }
+
+                // todo：观察文件大小，考虑是否使用缓冲区
+                var buffer = new byte[stream.Length];
+                await stream.ReadAsync(buffer);
+                return buffer;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "read slpk file error!");
+                throw new BusnessException(500, "read slpk file error!", ex);
+            }
+            finally
+            {
+                stream?.Dispose();
+                zip?.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// 判断路径是否存在
+        /// </summary>
+        /// <param name="path">路径 (文件或文件夹)</param>
+        /// <param name="isFile">路径是否指向文件</param>
+        /// <returns></returns>
+        private static bool ExistPath(string path, out bool isFile)
+        {
+            isFile = false;
+
+            if (File.Exists(path))
+            {
+                isFile = true;
+                return true;
             }
 
-            var buffer = new byte[stream.Length];
-            await stream.ReadAsync(buffer);
-            return buffer;
+            return Directory.Exists(path);
         }
     }
 }
